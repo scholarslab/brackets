84c72bcdfe30d40239d206451b4252bbcd8e0036
Jeremy Boggs <jeremy@virginia.edu>
2022-05-02 11:59:59 -0400

    Dont' push this yet.

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,63 @@ $(".prompts").accordion({
   heightStyle: "content"
 });
 $(".prompts h4").css("cursor","pointer");
+
+// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+// starting from v2 you can add only the features you need reducing the bundle size
+$(document).ready(async function () {
+  await loadFull(tsParticles);
+
+  $("#banner")
+    .particles()
+    .init(
+      {
+        fpsLimit: 60,
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      },
+      function (container) {
+        // container is the particles container where you can play/pause or stop/start.
+        // the container is already started, you don't need to start it manually.
+      }
+    );
+});
