898c3f9a1f0dad862a4b9a9ec0c8eabc7efddbe6
Jeremy Boggs <jeremy@virginia.edu>
2022-05-03 12:13:05 -0400

    Adds particles JS script with Jennifer's font. Fixes #12.

--- js/scripts.js
+++ js/scripts.js
@@ -35,62 +35,60 @@ $(".prompts h4").css("cursor","pointer");
 
 // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
 // starting from v2 you can add only the features you need reducing the bundle size
-// $(document).ready(async function () {
-//   await loadFull(tsParticles);
+$(document).ready(async function () {
+  await loadFull(tsParticles);
 
-//   $("#banner")
-//     .particles()
-//     .init(
-//       {
-//         fpsLimit: 60,
-//         particles: {
-//           color: {
-//             value: "#ffffff",
-//           },
-//           links: {
-//             color: "#ffffff",
-//             distance: 150,
-//             enable: true,
-//             opacity: 0.5,
-//             width: 1,
-//           },
-//           collisions: {
-//             enable: true,
-//           },
-//           move: {
-//             direction: "none",
-//             enable: true,
-//             outModes: {
-//               default: "bounce",
-//             },
-//             random: false,
-//             speed: 2,
-//             straight: false,
-//           },
-//           number: {
-//             density: {
-//               enable: true,
-//               area: 800,
-//             },
-//             value: 80,
-//           },
-//           opacity: {
-//             value: 0.5,
-//           },
-//           shape: {
-//             options: {
-//               character
-//             type: "image"
-//           },
-//           size: {
-//             value: { min: 1, max: 5 },
-//           },
-//         },
-//         detectRetina: true,
-//       },
-//       function (container) {
-//         // container is the particles container where you can play/pause or stop/start.
-//         // the container is already started, you don't need to start it manually.
-//       }
-//     );
-// });
+  $("#banner")
+    .particles()
+    .init(
+      {
+        fpsLimit: 60,
+        particles: {
+          color: {
+            value: ['rgb(250,200,72)','rgb(8,130,116)','rgb(191,98,151)','rgb(191,50,52)']
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 0.5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1000,
+            },
+            value: 70,
+          },
+          opacity: {
+            value: 0.825,
+          },
+          shape: {
+            options: {
+              char: {
+                value: ['</>','*','[]','{j}','/*','<>','#'],
+                font: 'jen_hand_lightregular',
+                fill:true
+              }
+            },
+            type: 'char'
+          },
+          size: {
+            value: { min: 10, max: 20 },
+          },
+        },
+        detectRetina: true,
+      },
+      function (container) {
+        // container is the particles container where you can play/pause or stop/start.
+        // the container is already started, you don't need to start it manually.
+      }
+    );
+});
